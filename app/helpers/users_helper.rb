51c1ecd267d7e48030b4528e82c4c2a8a466ebbc
Chris Keathley <spyc3r@gmail.com>
2013-09-05 23:55:52 -0400

    adjust the default size for gravatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 
-  def gravatar_for(user, options = { :size => 175 })
+  def gravatar_for(user, options = { :size => 75 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
