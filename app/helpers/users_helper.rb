03d4a9157f6f6b6253003e3a3928a2d38a1ec5cb
Chris Keathley <spyc3r@gmail.com>
2013-09-06 11:25:27 -0400

    display users name

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 module UsersHelper
 
-  def gravatar_for(user, options = { :size => 75 })
+  def gravatar_url_for(user, options = { :size => 75 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  end
+
+  def gravatar_for(user, options = { :size => 75 })
+    gravatar_url = gravatar_url_for(user, options)
     image_tag(gravatar_url, :alt => user.name, class: "gravatar")
   end
 
