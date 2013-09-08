84e83c8546205b6c5017ba0036c3a3030a3b7908
Anthony Blardo <ablardo@gmail.com>
2013-09-07 20:02:02 -0400

    Shuffle users before presentation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_filter :ensure_correct_user, :only => [:show, :destroy]
 
   def index
-    @users = User.all
+    @users = User.all.shuffle
   end
 
   def show
