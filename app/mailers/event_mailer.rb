ae4629290af556bf58acefa16ad9c5a92212320e
Adam Haney <adam.haney@akimbo.io>
2013-11-03 19:50:09 -0500

    Removes example code from our pairing that accidentally got left in

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -6,8 +6,5 @@ class EventMailer < ActionMailer::Base
       :to => "chattanoogadeveloper@gmail.com",
       :subject => "#{@event.email} submitted a new event for your approval"
     )
-
-    some_other_function :the_argument => stuff
-
   end
 end
