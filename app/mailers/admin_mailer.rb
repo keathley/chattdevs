3ed26a2b0a3183d3c8a08ba5ed9df2e36dd005f6
Chris Keathley <spyc3r@gmail.com>
2014-01-24 23:34:09 -0500

    Update admin_mailer.rb

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,10 +1,10 @@
 class AdminMailer < ActionMailer::Base
-  default from: "spyc3r+chattdevs@gmail.com"
+  default from: "noreply@chattdevs.com"
 
   def new_job_posting(job)
     @job = job
     mail(
-      :to => "chattanoogadeveloper@gmail.com",
+      :to => "spyc3r+chattdevs@gmail.com",
       :subject => "#{@job.company_name} submitted a new job posting"
     )
   end
