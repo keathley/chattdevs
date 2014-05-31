638eab2fa39a6448de5f4f69b8d717ed5187d9e1
Chris Keathley <spyc3r@gmail.com>
2014-05-31 15:38:39 -0400

    Remove weird migration

--- db/migrate/20140315184901_add_provisioned_number_id_to_client_text_conversations.rb
+++
@@ -1,5 +0,0 @@
-class AddProvisionedNumberIdToClientTextConversations < ActiveRecord::Migration
-  def change
-    add_column :client_text_conversations, :provisioned_number_id, :integer
-  end
-end
